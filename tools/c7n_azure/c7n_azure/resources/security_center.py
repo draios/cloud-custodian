2384d3402de0fc317afe19b8e4991923ee7d41b4
Alex <alex.qiu@sysdig.com>
2021-07-08 16:39:44 -0700

    work on auto-provisioning-settings filter

--- tools/c7n_azure/c7n_azure/resources/security_center.py
+++ tools/c7n_azure/c7n_azure/resources/security_center.py
@@ -1,6 +1,6 @@
 from c7n_azure.provider import resources
 from c7n_azure.resources.arm import ArmResourceManager
-from c7n.filters.core import ValueFilter, type_schema
+from c7n.filters.core import Filter, ValueFilter, type_schema
 
 import logging
 log = logging.getLogger(__name__)
@@ -21,49 +21,44 @@ class SecurityCenter(ArmResourceManager):
         resource_type = 'Microsoft.Security/SecurityCenter'
 
 @SecurityCenter.filter_registry.register('auto-provisioning-settings')
-class AutoProvisioningSettingsFilter(ValueFilter):
+class AutoProvisioningSettingsFilter(Filter):
     """
-    Provides a value filter targeting the auditing policy of this
-    SQL Server.
+    Filter sql servers by whether they have recurring vulnerability scans
+    enabled.
 
-    Here is an example of the available fields:
+    :example:
 
-    .. code-block:: json
-
-      "state": "Enabled",
-      "storageEndpoint": "https://yourstorageendpoint.blob.core.windows.net/",
-      "retentionDays": 0,
-      "auditActionsAndGroups": [
-          "SUCCESSFUL_DATABASE_AUTHENTICATION_GROUP",
-          "FAILED_DATABASE_AUTHENTICATION_GROUP",
-          "BATCH_COMPLETED_GROUP"
-        ]
-
-    :examples:
-
-    Find SQL Servers with failed database login auditing enabled
+    Find SQL servers without vulnerability assessments enabled.
 
     .. code-block:: yaml
 
         policies:
-          - name: sqlserver-failed-login-audit
-            resource: azure.sqlserver
+          - name: sql-server-no-va
+            resource: azure.sql-server
             filters:
-              - type: uditing-policy
-                key: "auditActionsAndGroups"
-                op: contains
-                value: "FAILED_DATABASE_AUTHENTICATION_GROUP"
+              - type: vulnerability-assessment
+                enabled: false
 
     """
 
-    schema = type_schema('auto-provisioning-settings', rinherit=ValueFilter.schema)
+    schema = type_schema(
+        'auto-provisioning-settings',
+    )
 
-    def __call__(self, i):
+    log = logging.getLogger('custodian.azure.security-center.auto-provisioning-settings')
+
+    def __init__(self, data, manager=None):
+        super(AutoProvisioningSettingsFilter, self).__init__(data, manager)
+
+    def process(self, resources, event=None):
         client = self.manager.get_client()
-        settings = list(
-            client.auto_provisioning_settings
-            .list()
-        )
-        log.debug(settings)
-        return settings
-    
+
+        settings_iterator = client.auto_provisioning_settings.list()
+        settings_list = []
+        while True:
+            try:
+                settings_list.append(settings_iterator.next().serialize(True))
+            except StopIteration:
+                break
+
+        return settings_list
