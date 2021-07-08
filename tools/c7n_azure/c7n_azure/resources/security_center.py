0b102622ebd0e3eb5daa3428f0795841bb882bef
Alex <alex.qiu@sysdig.com>
2021-07-08 16:07:41 -0700

    work on auth-provisioning-settings filter

--- tools/c7n_azure/c7n_azure/resources/security_center.py
+++ tools/c7n_azure/c7n_azure/resources/security_center.py
@@ -1,5 +1,6 @@
 from c7n_azure.provider import resources
 from c7n_azure.resources.arm import ArmResourceManager
+from c7n.filters.core import ValueFilter, type_schema
 
 @resources.register('security-center')
 class SecurityCenter(ArmResourceManager):
@@ -15,3 +16,50 @@ class SecurityCenter(ArmResourceManager):
         #     'sku.name
         # )
         resource_type = 'Microsoft.Security/SecurityCenter'
+
+@SecurityCenter.filter_registry.register('auth-provisioning-settings')
+class ServerAuditingFilter(ValueFilter):
+    """
+    Provides a value filter targeting the auditing policy of this
+    SQL Server.
+
+    Here is an example of the available fields:
+
+    .. code-block:: json
+
+      "state": "Enabled",
+      "storageEndpoint": "https://yourstorageendpoint.blob.core.windows.net/",
+      "retentionDays": 0,
+      "auditActionsAndGroups": [
+          "SUCCESSFUL_DATABASE_AUTHENTICATION_GROUP",
+          "FAILED_DATABASE_AUTHENTICATION_GROUP",
+          "BATCH_COMPLETED_GROUP"
+        ]
+
+    :examples:
+
+    Find SQL Servers with failed database login auditing enabled
+
+    .. code-block:: yaml
+
+        policies:
+          - name: sqlserver-failed-login-audit
+            resource: azure.sqlserver
+            filters:
+              - type: uditing-policy
+                key: "auditActionsAndGroups"
+                op: contains
+                value: "FAILED_DATABASE_AUTHENTICATION_GROUP"
+
+    """
+
+    schema = type_schema('auth-provisioning-settings', rinherit=ValueFilter.schema)
+
+    def __call__(self, i):
+        client = self.manager.get_client()
+        settings = list(
+            client.auth_provisioning_settings
+            .list()
+        )
+
+        return settings
